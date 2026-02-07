bfa7a3e01e3382680802fd9d6973d9c8e435e4b0
Farrel Darian <62016900+fdarian@users.noreply.github.com>
2026-02-07 07:53:09 +0700

    feat: add daemon

---
+++ apps/cli/src/clipboard/daemon.ts
@@ -0,0 +1,71 @@
+import {
+	Command,
+	FileSystem,
+	HttpRouter,
+	HttpServerRequest,
+	HttpServerResponse,
+} from "@effect/platform";
+import { Effect, Schema } from "effect";
+import { HostConfig, SharedConfig } from "#src/config.ts";
+
+export namespace Config {
+	export const DaemonConfig = Schema.Struct({
+		pid: Schema.Number,
+		port: Schema.Number,
+	});
+	const ConfigFromJson = Schema.parseJson(DaemonConfig);
+	export type DaemonConfig = typeof DaemonConfig.Type;
+
+	const getPath = Effect.gen(function* () {
+		return `${yield* HostConfig.dir}/daemon.json`;
+	});
+
+	export const write = (input: DaemonConfig) =>
+		Effect.gen(function* () {
+			const fs = yield* FileSystem.FileSystem;
+			yield* fs.makeDirectory(yield* HostConfig.dir, { recursive: true });
+			yield* fs.writeFileString(yield* getPath, JSON.stringify(input));
+		});
+
+	export const load = Effect.gen(function* () {
+		const fs = yield* FileSystem.FileSystem;
+		const path = yield* getPath;
+		const content = yield* fs.readFileString(path);
+		return yield* Schema.decode(ConfigFromJson)(content);
+	});
+}
+
+export namespace SharedPort {
+	const getPath = Effect.gen(function* () {
+		return `${(yield* SharedConfig).dir}/daemon-port`;
+	});
+
+	export const read = Effect.gen(function* () {
+		const fs = yield* FileSystem.FileSystem;
+		const content = yield* fs.readFileString(yield* getPath);
+		return Number(content);
+	});
+
+	export const write = (port: number) =>
+		Effect.gen(function* () {
+			const fs = yield* FileSystem.FileSystem;
+			yield* fs.writeFileString(yield* getPath, String(port));
+		});
+}
+
+const getClipboard = Effect.gen(function* () {
+	const content = yield* Command.make("pbpaste").pipe(Command.string);
+	return HttpServerResponse.text(content);
+});
+
+const postClipboard = Effect.gen(function* () {
+	const request = yield* HttpServerRequest.HttpServerRequest;
+	const body = yield* request.text;
+	yield* Command.make("pbcopy").pipe(Command.feed(body), Command.exitCode);
+	return HttpServerResponse.text("ok");
+});
+
+export const server = HttpRouter.empty.pipe(
+	HttpRouter.get("/clipboard", getClipboard),
+	HttpRouter.post("/clipboard", postClipboard),
+);
