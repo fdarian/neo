ef1144630d20e69c0f8d8d3156bdee1661f01109
Farrel Darian <62016900+fdarian@users.noreply.github.com>
2026-02-07 07:55:24 +0700

    feat: clipboard daemon command

---
+++ apps/cli/src/commands/clipboard.ts
@@ -0,0 +1,32 @@
+import { Command as CliCommand } from "@effect/cli";
+import { HttpServer } from "@effect/platform";
+import { BunHttpServer } from "@effect/platform-bun";
+import { Effect } from "effect";
+import getPort from "get-port";
+import * as Daemon from "#src/clipboard/daemon.ts";
+
+const daemonCmd = CliCommand.make("daemon", {}, () =>
+	Effect.gen(function* () {
+		const port = yield* Effect.tryPromise(() => getPort());
+		yield* HttpServer.serveEffect(Daemon.server).pipe(
+			Effect.provide(
+				BunHttpServer.layer({ port: port, hostname: "0.0.0.0" }).pipe(
+					HttpServer.withLogAddress,
+				),
+			),
+		);
+
+		yield* Daemon.Config.write({ pid: process.pid, port });
+
+		yield* Effect.never;
+	}).pipe(Effect.scoped),
+).pipe(CliCommand.withDescription("Run clipboard bridge daemon"));
+
+export const clipboardCmd = CliCommand.make(
+	"clipboard",
+	{},
+	() => Effect.void,
+).pipe(
+	CliCommand.withDescription("Clipboard utilities"),
+	CliCommand.withSubcommands([daemonCmd]),
+);
